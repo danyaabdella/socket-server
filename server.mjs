23270c3e554d0ffa7919eb2d513148d9a26f660c
danya <danyaabdella@gmail.com>
2025-05-26 00:36:35 +0300

    add type

--- server.mjs
+++ server.mjs
@@ -1,5 +1,5 @@
 import { createServer } from "node:http";
-import initializeSocket from "./libs/socket.js";
+import initializeSocket from "./libs/socket";
 import next from "next";
 
 const dev = process.env.NODE_ENV !== "production";
